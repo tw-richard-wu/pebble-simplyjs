b530842a7088329b526193a38daa6b4674269388
tw-richard-wu <richard@wu.network>
2016-06-01 14:18:14 -0400

    WU test

--- weather.js
+++ weather.js
@@ -1,1 +1,7 @@
-Pebble.showSimpleNotificationOnPebble('Hello!', 'Notifications from JavaScript? Welcome to the future!');
+navigator.geolocation.getCurrentPosition(function(pos) {
+  var coords = pos.coords;
+  var weatherUrl = 'http://api.wunderground.com/api/faa942d410d5139c/hourly/q/NY/Cold_Spring_Harbor.json';
+  ajax({ url: weatherUrl, type: 'json' }, function(data) {
+    simply.text({ title: data.reponse.version, subtitle: data.response.version });
+  });
+});
