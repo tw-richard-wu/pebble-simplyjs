f9975001317c160964d0662d4c58d810cce9fcb8
tw-richard-wu <richard@wu.network>
2016-06-01 14:20:11 -0400

    Added fullscreen/scrollable

--- weather.js
+++ weather.js
@@ -1,3 +1,5 @@
+simply.fullscreen(true);
+simply.scrollable(true);
 navigator.geolocation.getCurrentPosition(function(pos) {
   var coords = pos.coords;
   var weatherUrl = 'http://api.wunderground.com/api/faa942d410d5139c/hourly/q/NY/Cold_Spring_Harbor.json';
