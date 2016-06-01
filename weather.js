24df6b9eb87cbf4719fba1c405db396f3a061e1c
tw-richard-wu <richard@wu.network>
2016-06-01 15:02:46 -0400

    Added geolocation

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ simply.fullscreen(true);
 simply.scrollable(true);
 navigator.geolocation.getCurrentPosition(function(pos) {
   var coords = pos.coords;
-  var weatherUrl = 'http://api.wunderground.com/api/faa942d410d5139c/hourly/q/NY/Cold_Spring_Harbor.json';
+  var weatherUrl = 'http://api.wunderground.com/api/faa942d410d5139c/hourly/q/' + coords.latitude + ',' + coords.longitude + '.json';
   var textbody = '';
   ajax({ url: weatherUrl, type: 'json' }, function(data) {
     for (var i = 0; i < 12; i++) {
