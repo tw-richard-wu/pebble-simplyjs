6dc2f9d77f834f35a82b81adda13fb649b87c216
tw-richard-wu <richard@wu.network>
2016-06-01 14:26:16 -0400

    Fixed typo

--- weather.js
+++ weather.js
@@ -4,6 +4,6 @@ navigator.geolocation.getCurrentPosition(function(pos) {
   var coords = pos.coords;
   var weatherUrl = 'http://api.wunderground.com/api/faa942d410d5139c/hourly/q/NY/Cold_Spring_Harbor.json';
   ajax({ url: weatherUrl, type: 'json' }, function(data) {
-    simply.text({ title: data.reponse.version, subtitle: data.response.version });
+    simply.text({ title: data.response.version, subtitle: data.response.version });
   });
 });
