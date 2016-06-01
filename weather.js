4cc960b3211b17148dde9f05e1bf76398d32a93e
tw-richard-wu <richard@wu.network>
2016-06-01 14:38:33 -0400

    Added hour

--- weather.js
+++ weather.js
@@ -3,7 +3,11 @@ simply.scrollable(true);
 navigator.geolocation.getCurrentPosition(function(pos) {
   var coords = pos.coords;
   var weatherUrl = 'http://api.wunderground.com/api/faa942d410d5139c/hourly/q/NY/Cold_Spring_Harbor.json';
+  var textbody = '';
   ajax({ url: weatherUrl, type: 'json' }, function(data) {
-    simply.text({ title: data.response.version, subtitle: data.response.version });
+    for (var i = 0; i < 12; i++) {
+      textbody = textbody + 'data.hourly_forecast.[i].FCTTIME.hour_padded\n';
+    }
+    simply.body(textbody, true);
   });
 });
