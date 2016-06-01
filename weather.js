26acd460b8b3cb9ac951d2bc165cf5eac19c62fb
tw-richard-wu <richard@wu.network>
2016-06-01 14:15:05 -0400

    Update with generic notification

--- weather.js
+++ weather.js
@@ -1,8 +1,1 @@
-navigator.geolocation.getCurrentPosition(function(pos) {
-  var coords = pos.coords;
-  var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
-      'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
-  ajax({ url: weatherUrl, type: 'json' }, function(data) {
-    simply.text({ title: data.name, subtitle: data.main.temp });
-  });
-});
+Pebble.showSimpleNotificationOnPebble('Hello!', 'Notifications from JavaScript? Welcome to the future!');
