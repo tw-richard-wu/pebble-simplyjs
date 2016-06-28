0ca7562e0a363ba156b9fe9edf2ce414daf22f94
Richard Wu <richard@wu.network>
2016-06-28 18:32:43 -0400

    Added 3-day forecast

--- weather.js
+++ weather.js
@@ -3,9 +3,10 @@ simply.scrollable(true);
 simply.body('Obtaining position...', true);
 navigator.geolocation.getCurrentPosition(function(pos) {
   var coords = pos.coords;
-  var weatherUrl = 'http://api.wunderground.com/api/faa942d410d5139c/hourly/q/' + coords.latitude + ',' + coords.longitude + '.json';
+  var hourlyUrl = 'http://api.wunderground.com/api/faa942d410d5139c/hourly/q/' + coords.latitude + ',' + coords.longitude + '.json';
+  var forecastUrl = 'http://api.wunderground.com/api/faa942d410d5139c/forecast/q/' + coords.latitude + ',' + coords.longitude + '.json';
   var textbody = '';
-  ajax({ url: weatherUrl, type: 'json' }, function(data) {
+  ajax({ url: hourlyUrl, type: 'json' }, function(data) {
     for (var i = 0; i < 12; i++) {
       textbody = textbody +
                   data.hourly_forecast[i].FCTTIME.hour_padded + ':' +
@@ -14,6 +15,17 @@ navigator.geolocation.getCurrentPosition(function(pos) {
                   data.hourly_forecast[i].icon.replace('mostly', 'm ').replace('partly', 'p ').replace('chance', 'ch ').substring(0,10) +
                   '\n';
     }
-    simply.body(textbody, true);
   });
+  textbody = textbody + '\n';
+  ajax({ url: forecastUrl, type: 'json' }, function(data) {
+    for (var i = 0; i < 3; i++) {
+      textbody = textbody +
+                  data.forecast.simpleforecast.forecastday[i].date.day + ':' +
+                  data.forecast.simpleforecast.forecastday[i].high.fahrenheit + ' ' +
+                  data.forecast.simpleforecast.forecastday[i].low.fahrenheit + ' ' +
+                  data.forecast.simpleforecast.forecastday[i].icon.replace('mostly', '').replace('partly', '').replace('chance', '').substring(0,10) +
+                  '\n';
+    }
+  });
+  simply.body(textbody, true);
 });
